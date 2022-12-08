05d3aa9f4a1c8c79e98a219485271081017d11ae
Cubas-G <fredyscubas00@gmail.com>
2022-12-08 05:27:46 -0600

    creando los componetes para trabajar el crud con students

--- config.ts
+++ config.ts
@@ -1,12 +1,10 @@
 export default {
-    appPort: process.env.APP_PORT || 3000,
+     appPort: process.env.APP_PORT || 3000,
     secret: process.env.SECRET || 'secret',
     db: {
         // si la conexion es segura faltaria agregar una contraseña
-        host: process.env.DB_HOST || 'cluster0.prmja.mongodb.net',
+        host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 27017,
-        name: process.env.DB_NAME || 'test',
-        username: process.env.DB_USER || 'dsalinas',
-        password: process.env.DB_PASS || 'NgpE77KF3o46SOVc'
+        name: process.env.DB_NAME || 'test'
     }
 };
