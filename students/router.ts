d3e04127d5d00d6343e74c092f86552630a4e4ca
Cubas-G <fredyscubas00@gmail.com>
2022-12-09 16:09:42 -0600

    modificaciones en la app en el router

--- students/router.ts
+++ students/router.ts
@@ -6,8 +6,15 @@ const router = Router();
 
 
 router.get("/", requireAuth, async (req: Request, res: Response, next: NextFunction) => {
-    const students = await controller.list();
-    res.json(students);
+    const list = await controller.list();
+    res.json({
+        data: list,
+        metadata: {
+            nextPage: 2,
+            currentPage: 1,
+            perPage: 10
+        }
+    });
 });
 
 router.post("/", requireAuth, async (req: Request, res: Response, next: NextFunction) => {
